81464a618035a0b31db8cf0a8467051e9030b65b
Federico Ficarelli <federico.ficarelli@gmail.com>
2015-12-29 10:35:03 +0100

    Added apidoc tests.

---
+++ tests/fixtures.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+
+from pyramid import *  # this is going to test package's __init__ exports
+
+__all__ = (
+    'flags',
+    'command',
+    'packagedir',
+)
+
+apidoc_command_lines = [
+    ("simple", '-o {outdir} {package}'),
+]
+
+apidoc_commands = [
+    ("sphinx", 'sphinx-apidoc'),
+]
+
+testpackages = [
+    ("testpackage", 'testdata/testpackage'),
+]
+
+
+@pytest.fixture(params=[e[1] for e in apidoc_command_lines], ids=[e[0] for e in apidoc_command_lines])
+def flags(request):
+    return request.param
+
+
+@pytest.fixture(params=[e[1] for e in apidoc_commands], ids=[e[0] for e in apidoc_commands])
+def command(request):
+    return request.param
+
+
+@pytest.fixture(params=[e[1] for e in testpackages], ids=[e[0] for e in testpackages])
+def packagedir(request):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), request.param)
---
+++ tests/test_apidoc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+import filecmp
+import subprocess
+
+# NB: due to the discovery mechanism carried out by py.test, we can import test modules
+# in the same way as if they were proper modules (even if no __init__.py is present).
+# For further details: https://pytest.org/latest/goodpractises.html
+from fixtures import *
+
+
+def test_compare(flags, command, packagedir, tmpdir):
+    # Create output directories
+    my_outdir = str(tmpdir.join('pyramid'))
+    theirs_outdir = str(tmpdir.join('compare'))
+    os.makedirs(my_outdir)
+    os.makedirs(theirs_outdir)
+    # Create command lines
+    my_cmdline = 'pyramid-apidoc ' + flags.format(outdir=my_outdir, package=os.path.abspath(packagedir))
+    theirs_cmdline = command + ' ' + flags.format(outdir=theirs_outdir, package=os.path.abspath(packagedir))
+    # Run commands
+    # subprocess.call(my_cmdline.split())
+    subprocess.call(theirs_cmdline.split())
+    # Compare
+    cmp = filecmp.dircmp(str(my_outdir), str(theirs_outdir))
+    assert len(cmp.left_only) == 0
+    assert len(cmp.right_only) == 0
+    assert len(cmp.funny_files) == 0
+    assert len(cmp.diff_files) == 0
+    cmp.report_full_closure()
